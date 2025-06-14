1d4af1c43e6975f940dd261a08f618be900c77d0
SEOKKAMONI <sj01048260279@gmail.com>
2025-06-14 21:24:34 +0900

    feat: Splash 화면 개발

--- app/app/_layout.tsx
+++ app/app/_layout.tsx
@@ -1,10 +1,49 @@
 import { Stack } from 'expo-router';
 import { SafeAreaView, StyleSheet } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import * as SplashScreen from "expo-splash-screen";
+import { useCallback, useEffect, useState } from 'react';
+import { sleep } from '@/src/utils/sleep';
+
+SplashScreen.preventAutoHideAsync();
+
+SplashScreen.setOptions({
+  duration: 1000,
+  fade: true,
+});
 
 export default function RootLayout() {
+  const [isReady, setIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await sleep(2000);
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (isReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [isReady]);
+
+  if (!isReady) {
+    return null;
+  }
+  
   return (
-    <GestureHandlerRootView style={styles.container}>
+    <GestureHandlerRootView 
+      style={styles.container}
+      onLayout={onLayoutRootView} 
+    >
       <SafeAreaView style={styles.safeAreaView}>
         <Stack>
           <Stack.Screen
